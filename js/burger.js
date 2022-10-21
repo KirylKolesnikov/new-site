7a2d116ea5437fbee8bae40c36c6fee6b372d200
Kiryl Kolesnikov <ziga6552@gmail.com>
2022-10-21 12:42:40 +0300

    first commit

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('.menu__icon')
+const menu = document.querySelector('.menu__list')
+
+if (menuBtn && menu) {
+	menuBtn.addEventListener('click', () => {
+		menuBtn.classList.toggle('active')
+		menu.classList.toggle('active')
+	})}
---
+++ js/main.js
@@ -0,0 +1,76 @@
+const menuBtn =document.querySelector('.menu-icon')
+const menu =document.querySelector('.menu-list')
+const body =document.body
+if(menuBtn && menu) {
+  menuBtn.addEventListener('click', () =>{
+    menuBtn.classList.toggle('active')
+    menu.classList.toggle('active')
+    body.classList.toggle('noscroll')
+  })
+  menu.querySelectorAll('a').forEach(link => {
+		link.addEventListener('click', () => {
+			menuBtn.classList.toggle('active')
+			menu.classList.toggle('active')
+		})
+	})
+}
+
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', e => {
+		e.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+})
+
+
+
+
+AOS.init()
+
+$(function(){
+    $('.rev-slide').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // Адаптив
+          
+        ]
+      });
+})
+
+
+
