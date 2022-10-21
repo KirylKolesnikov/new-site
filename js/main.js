4d1101563096d044bc43e4d980d3c5ef0a6b5a16
Kiryl Kolesnikov <ziga6552@gmail.com>
2022-10-21 14:38:47 +0300

    end-project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-const menuBtn =document.querySelector('.menu-icon')
-const menu =document.querySelector('.menu-list')
-const body =document.body
+const menuBtn = document.querySelector('.menu-icon')
+const menu = document.querySelector('.menu-list')
+
+const body = document.body
+
+
+
 if(menuBtn && menu) {
   menuBtn.addEventListener('click', () =>{
     menuBtn.classList.toggle('active')
     menu.classList.toggle('active')
-    body.classList.toggle('noscroll')
+    body.classList.toggle('lock')
   })
   menu.querySelectorAll('a').forEach(link => {
 		link.addEventListener('click', () => {
@@ -13,6 +17,13 @@ if(menuBtn && menu) {
 			menu.classList.toggle('active')
 		})
 	})
+  menu.querySelectorAll('a').forEach(link =>{
+    link.addEventListener('click', () =>{
+      menuBtn.classList.remove('active')
+      menu.classList.remove('active')
+      body.classList.remove('lock')
+    })
+  })
 }
 
 
